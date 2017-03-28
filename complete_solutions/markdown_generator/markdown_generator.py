7acc575b360257e27f7f9b947cc69a906bcf94e0
Martin Bäumer <martin.baeumer@gmail.com>
2017-03-28 23:12:53 +0200

    started with a markdown blog generator

---
+++ complete_solutions/markdown_generator/markdown_generator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import sys
+
+def main(argv):
+  if validateCliArgs(argv):
+    writeMarkDownBlogFile(argv[1])
+
+def validateCliArgs(argv):
+  if len(argv) < 2:
+    print("Sorry, you have not provided the correct arguments!")
+    return False
+  targetFileName = str(argv[1])
+  if not targetFileName.endswith(".markdown"):
+    print("The filename '%s' is not correct!" % (targetFileName))
+    return False
+  return True
+
+def getHeaderDashes():
+  return "---\n"
+
+def getLayoutString(layout):
+  return "layout: " + layout + "\n"
+
+def getTitleString(title):
+  return "title: " + title + "\n"
+
+def getDateString(date):
+  return "date: " + date + "\n"
+
+def getCategoryString(category):
+  return "categories: " + category + "\n"
+
+def writeMarkDownBlogFile(filename):
+  print("Wrting to file %s" % (filename))  
+  file = open(filename, "w")
+  file.write(getHeaderDashes())
+  file.write(getLayoutString("post"))
+  file.write(getTitleString("test-title"))
+  file.write(getDateString("2017-03-28 22:00:00"))
+  file.write(getCategoryString("jekyll update"))
+  file.write(getHeaderDashes())
+  file.close()
+  print("...file creation completed.")
+
+if __name__ == '__main__':
+  main(sys.argv)
