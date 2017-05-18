4ee8df688ed5ca66a5c0210037370ea29cdf123a
Martin Bäumer <martin.baeumer@gmail.com>
2017-05-18 21:09:59 +0200

    block 2: cleaned up enum demo

--- block2-datatypes/enums/enum_demo.py
+++ block2-datatypes/enums/enum_demo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 from status import Status
 
-print("Demo of enumms in python")
+print("Demo of enums in python")
 print("")
 
-print("Step 1: creating a variable of type Status...")
+print("Step 1: creating a variable of type Status with value OPEN")
 status  = Status.OPEN
 print("Status: " + str(status))
 
@@ -18,11 +18,14 @@ print("Step 3: See all available values for the enumeration status...")
 for status in Status:
   print(status)
 
-print("Step 4: accessing the enum by value...")
+print("Step 4: accessing the enum by numeric value: Status(1)")
 print(Status(1))
-print("Step 5: accessing an enum value that does not exist...")
-#print(Status(4))
-print("Step 6: checking if a potential member  is really a member of an enum...")
+print("Step 5: accessing an enum value that does not exist: Status(4) should not exist")
+try:
+  print(Status(4))
+except ValueError:
+  print("Status(4) is not valid!")
+print("Step 6: checking if a potential member is really a member of an enum...")
 print(isinstance(Status.OPEN, Status))
 print("Step 7: Retrieving the value of an enum member...")
-print("Status.OPEN has the value %d " % (Status.OPEN.value))
+print("Status.OPEN has the numeric value %d " % (Status.OPEN.value))
