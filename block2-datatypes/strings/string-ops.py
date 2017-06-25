26eb609b20832055937fb44ccdf6842837dee598
Martin Bäumer <martin.baeumer@gmail.com>
2017-06-25 21:02:51 +0200

    added more on string operations

---
+++ block2-datatypes/strings/string-ops.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+print("Demo of string operations")
+print("")
+lorem = "lorem ipsum"
+print("Dummy string:  ", lorem)
+print("Length: ",len(lorem) )
+print("Capitalized: " + lorem.capitalize())
+print("endswith 'ipsum': " + str(lorem.endswith("ipsum")))
+print("islower: " + str(lorem.islower()))
+print("isnumeric: " + str(lorem.isnumeric()))
+print("isspace: " + str(lorem.isspace()))
+
+lorem2 = "lorem ipsum"
+print("Testing for string equality")
+if lorem == 'lorem ipsum':
+  print("The strings are equal")
+else:
+  print("The strings are not equal")
+
+print("Iterate through a string")
+i = 0
+while i < len(lorem):
+  print(lorem[i])
+  i = i +1
+
+print("Splitting a string")
+splitted = lorem.split(" ")
+for word in splitted:
+  print(word)
+
+print("Removing a character in a string")
+new_lorem = lorem.replace("m", "")
+print("new string after removal: %s" % (new_lorem))
+
+print("Inserting a character into a string")
+new_lorem = lorem[:5] + "-" + lorem[6:]
+print("new string after modification: %s" % (new_lorem))
+
