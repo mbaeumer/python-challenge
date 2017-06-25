17d4ddb74e8fd2d16f4d4498bcaf2e9408f6ab1e
Martin Bäumer <martin.baeumer@gmail.com>
2017-06-25 21:01:19 +0200

    added more on enum

--- block2-datatypes/enums/enum_demo.py
+++ block2-datatypes/enums/enum_demo.py
@@ -8,6 +8,10 @@ print("Step 1: creating a variable of type Status with value OPEN")
 status  = Status.OPEN
 print("Status: " + str(status))
 
+print("Step 2: Setting the status with a numeric value")
+another_status = Status(2)
+print(another_status)
+
 print("Step 2: checking the value of the enum variable...")
 if status == Status.COMPLETED:
   print("Congrats, the task is completed")
@@ -25,7 +29,9 @@ try:
   print(Status(4))
 except ValueError:
   print("Status(4) is not valid!")
+
 print("Step 6: checking if a potential member is really a member of an enum...")
 print(isinstance(Status.OPEN, Status))
+
 print("Step 7: Retrieving the value of an enum member...")
 print("Status.OPEN has the numeric value %d " % (Status.OPEN.value))
