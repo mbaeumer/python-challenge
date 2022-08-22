5b9129d6597a938cd348ee7ed139073c1a065278
Martin Bäumer <martin.baeumer@gmail.com>
2022-08-22 23:08:48 +0200

    added support for recursive counting

--- complete_solutions/file_size_counter/file_size_counter.py
+++ complete_solutions/file_size_counter/file_size_counter.py
@@ -1,18 +1,32 @@
 import glob
-import os
+from os import stat
+from os.path import isdir
 
 
-def count_total_file_size(base_dir):
-    files = glob.glob(base_dir + '/**/*.*', recursive=True)
+def count_total_file_size(base_dir, recursive):
+    if recursive:
+        files = glob.glob(base_dir + '/**/*.*', recursive=True)
+    else:
+        files = glob.glob(base_dir + '/*.*', recursive=False)
+
     total_size = 0
     for file in files:
         total_size = total_size + get_file_stats(file)
     filesize_in_megabytes = total_size % 1024
-    print("Total file size: %d" % (filesize_in_megabytes))
+    print("Total file size of %s: %d" % (base_dir, filesize_in_megabytes))
+    print("Total number of files of %s: %d" % (base_dir, len(files)))
 
 def get_file_stats(filename):
-  statresult = os.stat(filename)
-  return statresult.st_size
+    filesize = 0
+    try:
+        if not isdir(filename):
+            statresult = stat(filename)
+            filesize = statresult.st_size
+    except FileNotFoundError as e:
+      filesize = 0
+    return filesize
 
 if __name__ == '__main__':
-    count_total_file_size('/Users/martinbaumer/Documents/fxlink-builds')
+    count_total_file_size('/Users/martinbaumer/Documents', False)
+    count_total_file_size('/Users/martinbaumer/Downloads', True)
+    count_total_file_size('/Users/martinbaumer/Desktop', False)
