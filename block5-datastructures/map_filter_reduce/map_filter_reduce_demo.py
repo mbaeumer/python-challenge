7ff17d42e4f224119c3c2998470ed5441340f65b
Martin Bäumer <martin.baeumer@gmail.com>
2022-10-29 23:30:18 +0200

    added examples for map and filter

---
+++ block5-datastructures/map_filter_reduce/map_filter_reduce_demo.py
@@ -0,0 +1,25 @@
+
+
+def multiply(value):
+    return value * value
+
+def map_demo():
+    list = [1,2,3,4]
+    mapped_list = map(multiply, list)
+
+    for item in mapped_list:
+        print(item)
+
+def even_number(value):
+    return value % 2 == 0
+
+def filter_demo():
+    list = [1, 2, 3, 4]
+    even_numbers = filter(even_number, list)
+
+    for item in even_numbers:
+        print(item)
+
+if __name__ == '__main__':
+    map_demo()
+    filter_demo()
