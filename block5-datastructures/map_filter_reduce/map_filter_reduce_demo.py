545fe1238a4d34ecfaae671621acb0a379155f52
Martin Bäumer <martin.baeumer@gmail.com>
2022-10-29 23:45:22 +0200

    added example for reduce

--- block5-datastructures/map_filter_reduce/map_filter_reduce_demo.py
+++ block5-datastructures/map_filter_reduce/map_filter_reduce_demo.py
@@ -1,4 +1,4 @@
-
+from functools import reduce
 
 def multiply(value):
     return value * value
@@ -20,6 +20,16 @@ def filter_demo():
     for item in even_numbers:
         print(item)
 
+def add(a, b):
+    return a+ b
+
+def reduce_demo():
+    list = [1, 2, 3, 4]
+    sum = reduce(add, list)
+    print("The sum of all values in the list: %d" % sum)
+
+
 if __name__ == '__main__':
     map_demo()
     filter_demo()
+    reduce_demo()
