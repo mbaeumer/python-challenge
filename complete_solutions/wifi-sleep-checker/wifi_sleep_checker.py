b00e40e201bd2a3f51282e15cd42c865df5868b6
Martin Bäumer <martin.baeumer@gmail.com>
2023-02-13 01:17:40 +0100

    added wifi status class; adjusted sleep time check

--- complete_solutions/wifi-sleep-checker/wifi_sleep_checker.py
+++ complete_solutions/wifi-sleep-checker/wifi_sleep_checker.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import subprocess
+from wifi_status import WifiStatus
 
 def get_airport_status():
     command = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I".split()
@@ -19,7 +20,7 @@ def get_wifi_status():
     airport_status = get_airport_status()
     key_index = airport_status.find("SSID")
     if key_index == -1:
-        return "not connected"
+        return WifiStatus(False,"")
 
     rows = airport_status.split("\n")
     stripped_rows = [str(row).strip() for row in rows]
@@ -28,14 +29,30 @@ def get_wifi_status():
     ssid_key_value = str(row_with_ssid).split(":")
     ssid_value = ssid_key_value[1].strip()
 
-    return "connected to " + ssid_value
+    return WifiStatus(True, ssid_value)
     
 def is_time_to_sleep(date):
     #date = datetime.datetime.now()
-    return date.hour >= 22 and date.minute >= 30
+    return (date.hour >= 22 and date.minute >= 30) or date.hour <= 4
+
+def disconnect_from_wifi():
+    command = "networksetup -setnetworkserviceenabled Wi-Fi off".split()
+    cmd = subprocess.run(
+        command, stdout=subprocess.PIPE
+    )
+# TODO:
+# Command for disconnecting from wifi
+# adjust 'is_time_to_sleep' function
 
 if __name__ == '__main__':
     while True:
         time.sleep(5)
-        print(get_wifi_status())
+        wifi_status = get_wifi_status()
+        print(wifi_status.is_connected)
+        print(wifi_status.ssid)
+        if is_time_to_sleep(datetime.datetime.now()):
+            print("now it is time to go to bed!")
+            disconnect_from_wifi()
+
         break
+
---
+++ complete_solutions/wifi-sleep-checker/wifi_status.py
@@ -0,0 +1,5 @@
+
+class WifiStatus:
+    def __init__(self, is_connected, ssid):
+        self.is_connected = is_connected
+        self.ssid = ssid
