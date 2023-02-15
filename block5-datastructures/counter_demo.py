5999351bb1af49341eabdff6f673bfc6e9395bb4
Martin Bäumer <martin.baeumer@gmail.com>
2023-02-15 01:13:14 +0100

    added initial examples of Counter

---
+++ block5-datastructures/counter_demo.py
@@ -0,0 +1,75 @@
+from collections import Counter
+
+def create_counter_from_list():
+    counter = Counter(['a', 'b', 'a'])
+    print(counter)
+
+def create_counter_with_values():
+    counter = Counter(a=2, b=1)
+    print(counter)
+
+def create_counter_from_string():
+    counter = Counter('aba')
+    print(counter)
+
+# delete counter
+# elements
+# most common
+# subtract
+# update
+# counter arithmetic
+
+def get_count():
+    counter = Counter('aba')
+    print(counter['a'])
+
+def set_count():
+    counter = Counter('aba')
+    counter['a'] = 5
+    print(counter['a'])
+
+def delete_counter():
+    counter = Counter('aba')
+    del counter['b']
+    print(counter)
+
+def get_elements():
+    counter = Counter('aba')
+    print(counter.elements())
+
+def most_common():
+    counter = Counter('aba')
+    print(counter.most_common())
+
+def subtract():
+    counterA = Counter('aba')
+    counterB = Counter('bca')
+    counterA.subtract(counterB)
+    print(counterA)
+
+    counterA = Counter('aba')
+    counterB = Counter('abb')
+    counterA.subtract(counterB)
+    print(counterA)
+
+def update():
+    counterA = Counter('aba')
+    counterB = Counter('abb')
+    counterA.update(counterB)
+    print(counterA)
+
+
+
+# source: https://www.digitalocean.com/community/tutorials/python-counter-python-collections-counter
+if __name__ == '__main__':
+    create_counter_from_list()
+    create_counter_with_values()
+    create_counter_from_string()
+
+    get_count()
+    set_count()
+    delete_counter()
+    get_elements()
+    most_common()
+    subtract()
+    update()
