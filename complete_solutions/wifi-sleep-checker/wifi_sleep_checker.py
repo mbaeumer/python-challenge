9b5431db883120619442cf01027b7199b4948511
Martin Bäumer <martin.baeumer@gmail.com>
2023-04-07 17:53:00 +0200

    wifi sleep checker: added support for CLI args

--- complete_solutions/wifi-sleep-checker/wifi_sleep_checker.py
+++ complete_solutions/wifi-sleep-checker/wifi_sleep_checker.py
@@ -2,6 +2,16 @@ import datetime
 import time
 import subprocess
 from wifi_status import WifiStatus
+import re
+import sys
+
+
+def handle_cli_args(argv):
+    if len(argv) != 2:
+        print("Not enough arguments")
+        return False
+
+    return bool(re.search(r"^(0[1-9]|1[0-2]):([0-5][0-9])$", argv[1]))
 
 def get_airport_status():
     command = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I".split()
@@ -12,9 +22,6 @@ def get_airport_status():
     
     return output
   
-
-#def get_wifi_name():
-    
 
 def get_wifi_status():
     airport_status = get_airport_status()
@@ -30,21 +37,31 @@ def get_wifi_status():
     ssid_value = ssid_key_value[1].strip()
 
     return WifiStatus(True, ssid_value)
+
+def get_sleep_datetime(now, sleep_timestamp):
+    hour = str(sleep_timestamp).split(":")[0]
+    minute = str(sleep_timestamp).split(":")[1]
+    sleep_datetime_string = str(now.year) + "-" + str(now.month) + "-" + str(now.day) + " " + str(hour) + ":" + str(minute)
+    sleep_datetime = datetime.datetime.strptime(sleep_datetime_string, "%Y-%m-%d %H:%M")
+
+    if now > sleep_datetime:
+        sleep_datetime = sleep_datetime + datetime.timedelta(days=1)
+
+    return sleep_datetime
     
-def is_time_to_sleep(date):
-    #date = datetime.datetime.now()
-    return (date.hour >= 22 and date.minute >= 30) or date.hour <= 4
+def is_time_to_sleep(date, sleep_time):
+    return date > sleep_time
 
 def disconnect_from_wifi():
     command = "networksetup -setnetworkserviceenabled Wi-Fi off".split()
     cmd = subprocess.run(
         command, stdout=subprocess.PIPE
     )
-# TODO:
-# Command for disconnecting from wifi
-# adjust 'is_time_to_sleep' function
 
 if __name__ == '__main__':
+    if not handle_cli_args(sys.argv):
+        exit(1)
+    sleep_time = sys.argv[1]
     while True:
         time.sleep(5)
         wifi_status = get_wifi_status()
--- complete_solutions/wifi-sleep-checker/wifi_sleep_checker_test.py
+++ complete_solutions/wifi-sleep-checker/wifi_sleep_checker_test.py
@@ -1,15 +1,27 @@
 import unittest
 import datetime
 from wifi_sleep_checker import is_time_to_sleep
+from wifi_sleep_checker import get_sleep_datetime
 
 class SleepTimeTest(unittest.TestCase):
     def test_is_time_to_sleep_false(self):
         date = datetime.datetime.strptime("2017-07-01 15:34", "%Y-%m-%d %H:%M")
-        self.assertFalse(is_time_to_sleep(date))
+        sleep_datetime = datetime.datetime.strptime("2017-07-01 23:45", "%Y-%m-%d %H:%M")
+        self.assertFalse(is_time_to_sleep(date, sleep_datetime))
 
     def test_is_time_to_sleep_true(self):
         date = datetime.datetime.strptime("2017-07-01 23:55", "%Y-%m-%d %H:%M")
-        self.assertTrue(is_time_to_sleep(date))
+        sleep_datetime = datetime.datetime.strptime("2017-07-01 23:45", "%Y-%m-%d %H:%M")
+        self.assertTrue(is_time_to_sleep(date, sleep_datetime))
+
+    def test_get_sleep_time(self):
+        now = datetime.datetime.strptime("2023-03-28 15:34", "%Y-%m-%d %H:%M")
+        sleep_timestamp = "22:45"
+        expected_sleep_datetime = datetime.datetime.strptime("2023-03-28 22:45", "%Y-%m-%d %H:%M")
+        actual_sleep_datetime = get_sleep_datetime(now, sleep_timestamp)
+
+        self.assertEqual(actual_sleep_datetime, expected_sleep_datetime)
+
 
 
 if __name__ == '__main__':
