623ef473af7c09a34bcd6b95b92f7e921b0ce68d
Martin Bäumer <martin.baeumer@gmail.com>
2023-04-07 17:48:12 +0200

    refactored some regex examples

--- block10_regex/regex_demo.py
+++ block10_regex/regex_demo.py
@@ -2,25 +2,28 @@ import re
 
 #split
 def split_demo(string):
-    print(re.split(r"\W", string))
+    return re.split(r"\W", string)
 
 #match
 def match_demo(string):
-    print(re.match(r"\d+", string))
+    return bool(re.match(r"\d+", string))
+
+def match_span_demo(string):
+    return re.match(r"\d+", string).span()
 
 #search
-def search_demo():
-    print(re.search(r"\w+", "This is a sentence with characters and 0 numbers"))
+def search_demo(string):
+    return re.search(r"\w+", string)
 
 #findall
-def findall_demo():
-    print(re.findall(r"\d", "5446 is my number"))
+def findall_demo(string):
+    return re.findall(r"\d", string)
 
-def one_character_of_demo():
-    print(re.search(r"[a-d]", "abecd"))
+def one_character_of_demo(string):
+    return re.search(r"[a-d]", string)
 
-def one_character_except_demo():
-    print(re.search(r"[^a-d]", "abecd"))
+def one_character_except_demo(string):
+    return re.search(r"[^a-d]", string)
 
 def valid_minute_demo(time_string):
     match = re.match(r"^[0-5]",time_string)
@@ -35,15 +38,14 @@ def valid_minute_group_demo(time_string):
 if __name__ == '__main__':
     string = "This is a sentence with characters and 0 numbers"
     start_with_number = "5446 is my number"
-    split_demo(string)
-    split_demo(start_with_number)
-    match_demo(string)
-    match_demo(start_with_number)
-    search_demo()
-    findall_demo()
-    one_character_of_demo()
-    one_character_except_demo()
+    print(string + "->", split_demo(string))
+    print(start_with_number + "->", split_demo(start_with_number))
+    print(string + "->", match_demo(string))
+    print(start_with_number + "->", match_demo(start_with_number))
+    print(start_with_number + "->", match_span_demo(start_with_number))
+    print(string + "->", search_demo(string))
+    print(start_with_number + "->", findall_demo(start_with_number))
+    print("abecd ->", one_character_of_demo("abecd"))
+    print("abecd ->", one_character_except_demo("abecd"))
     print(valid_minute_demo(""))
     print(valid_minute_demo("0"))
-    print(valid_minute_group_demo("0"))
-    print(valid_minute_group_demo("6"))
