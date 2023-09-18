cdd6e76870ed39c5effdb7e9fb3eb0faea2d1650
Martin Bäumer <martin.baeumer@gmail.com>
2023-09-19 00:56:51 +0200

    file size counter: support to find the largest and smallest file in a folder

--- complete_solutions/file_size_counter/file_size_counter.py
+++ complete_solutions/file_size_counter/file_size_counter.py
@@ -59,6 +59,13 @@ def show_quick_overview():
     count_total_file_size('/Users/martinbaumer/Downloads', False)
     count_total_file_size('/Users/martinbaumer/Desktop', False)
 
+def get_files_in_directory(base_dir, recursive):
+    if recursive:
+        return glob.glob(base_dir + '/**/*.*', recursive=True)
+    else:
+        return glob.glob(base_dir + '/*.*', recursive=False)
+
+
 def count_total_file_size(base_dir, recursive):
     if recursive:
         files = glob.glob(base_dir + '/**/*.*', recursive=True)
@@ -85,7 +92,31 @@ def get_file_stats(filename):
 def show_detailed_overview(folder):
     print('This is a detailed overview on %s'% (folder))
 
+    largest_file = get_largest_file(folder)
+    print('Largest file: %s, file size: %d' % (largest_file , get_file_stats(largest_file) % 1024))
 
+    smallest_file = get_smallest_file(folder)
+    print('Smallest file: %s, file size: %d' % (smallest_file, get_file_stats(smallest_file) % 1024))
+
+def get_largest_file(base_dir):
+    files = get_files_in_directory(base_dir, False)
+    max_file_size = 0
+    largest_file = ''
+    for file in files:
+        if get_file_stats(file) > max_file_size:
+            largest_file = file
+            max_file_size = get_file_stats(file)
+    return largest_file
+
+def get_smallest_file(base_dir):
+    files = get_files_in_directory(base_dir, False)
+    min_file_size = 30000000
+    smallest_file = ''
+    for file in files:
+        if get_file_stats(file) < min_file_size:
+            smallest_file = file
+            min_file_size = get_file_stats(file)
+    return smallest_file
 
 if __name__ == '__main__':
     show_menu()
