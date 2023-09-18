a51da31dfcfac9667f6d9838b9611666cd1b1d17
Martin Bäumer <martin.baeumer@gmail.com>
2023-09-18 22:56:25 +0200

    lay the ground work for a detailed folder view

--- complete_solutions/file_size_counter/file_size_counter.py
+++ complete_solutions/file_size_counter/file_size_counter.py
@@ -5,7 +5,8 @@ from os.path import isdir
 def show_menu():
     menu_options = {
         1: 'Quick overview',
-        2: 'Exit'
+        2: 'Detailed overview',
+        3: 'Exit'
     }
 
     while (True):
@@ -21,8 +22,37 @@ def show_menu():
 
         if userinput == 1:
             show_quick_overview()
+        elif userinput == 2:
+            try:
+                folder = get_folder_selection()
+                show_detailed_overview(folder)
+            except ValueError:
+                continue
         else:
             exit(0)
+
+def get_folder_selection():
+    folder_options = {
+        1: '/Users/martinbaumer/Documents',
+        2: '/Users/martinbaumer/Downloads',
+        3: '/Users/martinbaumer/Desktop',
+        4: 'back to main menu'
+    }
+    while (True):
+        print("Select a folder")
+        print("---------")
+        for k in folder_options.keys():
+            print(k, "-", folder_options[k])
+        try:
+            userinput = int(input("Enter your choice: "))
+        except ValueError:
+            print("Please enter a number")
+            continue
+
+        if userinput == 4:
+            raise ValueError
+        else:
+            return folder_options[userinput]
 
 def show_quick_overview():
     count_total_file_size('/Users/martinbaumer/Documents', False)
@@ -51,6 +81,11 @@ def get_file_stats(filename):
     except FileNotFoundError as e:
       filesize = 0
     return filesize
+
+def show_detailed_overview(folder):
+    print('This is a detailed overview on %s'% (folder))
+
+
 
 if __name__ == '__main__':
     show_menu()
